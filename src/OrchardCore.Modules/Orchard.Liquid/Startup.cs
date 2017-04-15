d9d4d1c1df7956bb1107e6b3af4aa3539dd368e6
Sebastien Ros <sebastienros@gmail.com>
2017-04-14 18:39:25 -0700

    Creating LiquidPart

---
+++ src/OrchardCore.Modules/Orchard.Liquid/Startup.cs
@@ -0,0 +1,44 @@
+﻿using System.Security.Claims;
+using DotLiquid;
+using DotLiquid.NamingConventions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Modules;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Linq;
+using Orchard.ContentManagement;
+using Orchard.ContentManagement.Display.ContentDisplay;
+using Orchard.ContentManagement.Handlers;
+using Orchard.Data.Migration;
+using Orchard.Indexing;
+using Orchard.Liquid.Drivers;
+using Orchard.Liquid.Handlers;
+using Orchard.Liquid.Indexing;
+using Orchard.Liquid.Model;
+using System.Collections.Generic;
+using System;
+
+namespace Orchard.Liquid
+{
+    public class Startup : StartupBase
+    {
+        public override void ConfigureServices(IServiceCollection services)
+        {
+            services.AddLiquid();
+
+            // Liquid Part
+            services.AddScoped<IContentPartDisplayDriver, LiquidPartDisplay>();
+            services.AddSingleton<ContentPart, LiquidPart>();
+            services.AddScoped<IDataMigration, Migrations>();
+            services.AddScoped<IContentPartIndexHandler, LiquidPartIndexHandler>();
+            services.AddScoped<IContentPartHandler, LiquidPartHandler>();
+
+            Template.RegisterSafeType(typeof(IContent), new string[] { nameof(IContent.ContentItem) });
+            Template.RegisterSafeType(typeof(ContentItem), new string[] { nameof(ContentItem.Content) });
+            Template.RegisterSafeType(typeof(ContentElement), new string[] { nameof(ContentElement.Content) }, o => (JObject)o);
+            Template.RegisterSafeType(typeof(JObject), new string[] { });
+            Template.RegisterSafeType(typeof(HttpContext), new string[] { nameof(HttpContext.User) });
+            Template.RegisterSafeType(typeof(ClaimsPrincipal), new string[] { nameof(ClaimsPrincipal.Identity) }, o => o.ToString());
+            Template.NamingConvention = new CSharpNamingConvention();
+        }
+    }
+}
---
+++ src/OrchardCore/Orchard.Liquid.Core/LiquidManager.cs
@@ -0,0 +1,35 @@
+﻿using DotLiquid;
+using System.Collections.Concurrent;
+
+namespace Orchard.Liquid
+{
+    public class LiquidManager : ILiquidManager
+    {
+        private static Template Empty = Template.Parse("");
+
+        private static ConcurrentDictionary<string, Template> _templates = new ConcurrentDictionary<string, Template>();
+        
+        public Template GetTemplate(string source)
+        {
+            if (_templates.TryGetValue(source, out var template))
+            {
+                return template;
+            }
+            else
+            {
+                try
+                {
+                    template = Template.Parse(source);
+                    template = _templates.GetOrAdd(source, template);
+                    return template;
+                }
+                catch
+                {
+                    return Empty;
+                }
+            }
+
+            
+        }
+    }
+}
