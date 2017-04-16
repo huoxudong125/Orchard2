3914c4d16aa2d3a39e2ef1ddfc24cf20c7b048e4
Sebastien Ros <sebastienros@gmail.com>
2017-04-16 11:47:08 -0700

    Adding support for JToken resolution

--- src/OrchardCore.Modules/Orchard.Liquid/Startup.cs
+++ src/OrchardCore.Modules/Orchard.Liquid/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using System.Security.Claims;
+using System.Security.Principal;
 using DotLiquid;
 using DotLiquid.NamingConventions;
 using Microsoft.AspNetCore.Http;
@@ -14,8 +15,8 @@ using Orchard.Liquid.Drivers;
 using Orchard.Liquid.Handlers;
 using Orchard.Liquid.Indexing;
 using Orchard.Liquid.Model;
-using System.Collections.Generic;
-using System;
+using Microsoft.AspNetCore.Http.Internal;
+using System.Linq;
 
 namespace Orchard.Liquid
 {
@@ -33,11 +34,55 @@ namespace Orchard.Liquid
             services.AddScoped<IContentPartHandler, LiquidPartHandler>();
 
             Template.RegisterSafeType(typeof(IContent), new string[] { nameof(IContent.ContentItem) });
-            Template.RegisterSafeType(typeof(ContentItem), new string[] { nameof(ContentItem.Content) });
-            Template.RegisterSafeType(typeof(ContentElement), new string[] { nameof(ContentElement.Content) }, o => (JObject)o);
-            Template.RegisterSafeType(typeof(JObject), new string[] { });
-            Template.RegisterSafeType(typeof(HttpContext), new string[] { nameof(HttpContext.User) });
-            Template.RegisterSafeType(typeof(ClaimsPrincipal), new string[] { nameof(ClaimsPrincipal.Identity) }, o => o.ToString());
+            Template.RegisterSafeType(typeof(ContentItem), new string[] {
+                nameof(ContentItem.Id),
+                nameof(ContentItem.ContentItemId),
+                nameof(ContentItem.Number),
+                nameof(ContentItem.Owner),
+                nameof(ContentItem.Author),
+                nameof(ContentItem.Published),
+                nameof(ContentItem.Latest),
+                nameof(ContentItem.ContentType),
+                nameof(ContentItem.CreatedUtc),
+                nameof(ContentItem.ModifiedUtc),
+                nameof(ContentItem.PublishedUtc)
+            });
+
+            Template.RegisterSafeType(typeof(ContentElement), new string[] {
+                nameof(ContentElement.Content),
+                nameof(ContentElement.ContentItem)
+            });
+            Template.RegisterSafeType(typeof(DefaultHttpContext), new string[] {
+                nameof(HttpContext.User),
+                nameof(HttpContext.Request)
+            });
+            Template.RegisterSafeType(typeof(DefaultHttpRequest), new string[] {
+                nameof(HttpRequest.Path),
+                nameof(HttpRequest.PathBase),
+                nameof(HttpRequest.Host),
+                nameof(HttpRequest.IsHttps),
+                nameof(HttpRequest.Method),
+                nameof(HttpRequest.Protocol),
+                nameof(HttpRequest.Query),
+                nameof(HttpRequest.QueryString),
+                nameof(HttpRequest.Form),
+                nameof(HttpRequest.Cookies),
+                nameof(HttpRequest.Headers)
+            });
+
+            Template.RegisterSafeType(typeof(HostString), p => p.ToString());
+            Template.RegisterSafeType(typeof(PathString), p => p.ToString());
+            Template.RegisterSafeType(typeof(QueryString), p => p.ToString());
+            Template.RegisterSafeType(typeof(IQueryCollection), p => ((IQueryCollection)p).ToDictionary(x => x.Key, y => y.Value));
+            Template.RegisterSafeType(typeof(IFormCollection), p => ((IFormCollection)p).ToDictionary(x => x.Key, y => y.Value));
+            Template.RegisterSafeType(typeof(IRequestCookieCollection), p => ((IRequestCookieCollection)p).ToDictionary(x => x.Key, y => y.Value));
+            Template.RegisterSafeType(typeof(IHeaderDictionary), p => ((IHeaderDictionary)p).ToDictionary(x => x.Key, y => y.Value));
+
+            Template.RegisterSafeType(typeof(IPrincipal), new string[] { nameof(IPrincipal.Identity) });
+            Template.RegisterSafeType(typeof(IIdentity), new string[] { nameof(IIdentity.Name) });
+
+            DotLiquid.Liquid.UseRubyDateFormat = false;
+
             Template.NamingConvention = new CSharpNamingConvention();
         }
     }
